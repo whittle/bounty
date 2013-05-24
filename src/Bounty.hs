36498d36227948a50017d15348093108efa33a6c
Jason Whittle <jason.whittle@gmail.com>
2013-05-24 21:48:49 +0000

    Implement really primitive get and set.

--- src/Bounty.hs
+++ src/Bounty.hs
@@ -1,16 +1,22 @@
 import Control.Monad
+import Control.Monad.IO.Class
+import Control.Monad.Trans.State
+import qualified Data.Map as Map
+import Network.Memcached.Command
 import Network.Memcached.Lexer (alexScanTokens)
 import Network.Memcached.Parser (parseCommand)
-import Network.Memcached.Command
-
-isQuit :: Command -> Bool
-isQuit QuitCommand = True
-isQuit _ = False
+import qualified Network.Memcached.Types as Mem
 
 main :: IO ()
 main = do
-  line <- getLine
-  let command = parseCommand $ alexScanTokens line
-  unless (isQuit command) $ do
-    print command
-    main
+  runStateT loop Map.empty
+  return ()
+
+loop :: Mem.MemStateM ()
+loop = do
+  command <- fmap (parseCommand . alexScanTokens) $ liftIO getLine
+  state <- get
+  let (msg, newState) = apply command state
+  put newState
+  when (showMsg command) $ liftIO $ putStrLn msg
+  unless (isQuit command) loop
--- src/Network/Memcached/Command.hs
+++ src/Network/Memcached/Command.hs
@@ -1,26 +1,53 @@
 module Network.Memcached.Command
        ( Command(..)
        , StatisticsOption(..)
+       , apply
+       , isQuit
+       , showMsg
        ) where
 
-data Command = SetCommand String Integer Integer Integer Bool
-             | AddCommand String Integer Integer Integer Bool
-             | ReplaceCommand String Integer Integer Integer Bool
-             | AppendCommand String Integer Integer Integer Bool
-             | PrependCommand String Integer Integer Integer Bool
-             | CasCommand String Integer Integer Integer Integer Bool
-             | DeleteCommand String (Maybe Integer) Bool
-             | IncrementCommand String Integer Bool
-             | DecrementCommand String Integer Bool
+import Data.List (intercalate)
+import qualified Data.Map as Map
+import Data.Map ((!))
+import Network.Memcached.Types
+
+-- | All the different commands that regular memcached accepts.
+data Command = SetCommand Key Flags Exptime Bytes Bool
+             | AddCommand Key Flags Exptime Bytes Bool
+             | ReplaceCommand Key Flags Exptime Bytes Bool
+             | AppendCommand Key Flags Exptime Bytes Bool
+             | PrependCommand Key Flags Exptime Bytes Bool
+             | CasCommand Key Flags Exptime Bytes CasUnique Bool
+             | DeleteCommand Key (Maybe Time) Bool
+             | IncrementCommand Key Integer Bool
+             | DecrementCommand Key Integer Bool
              | FlushAllCommand (Maybe Integer) Bool
-             | GetCommand [String]
-             | GetsCommand [String]
+             | GetCommand [Key]
+             | GetsCommand [Key]
              | StatisticsCommand (Maybe StatisticsOption)
              | VersionCommand
-             | VerbosityCommand Integer
+             | VerbosityCommand VerbosityLevel
              | QuitCommand
              deriving Show
 
+apply :: Command -> MemState -> (String, MemState)
+apply (SetCommand k _ _ b _) s = ("Set " ++ k ++ " to " ++ show b, Map.insert k b s)
+apply (GetCommand ks) s = (showReverse s ks, s)
+apply _ s = ("No action taken: " ++ show s, s)
+
+showReverse :: MemState -> [Key] -> String
+showReverse m = intercalate " " . reverse . map (\k -> show (m ! k))
+
+isQuit :: Command -> Bool
+isQuit QuitCommand = True
+isQuit _ = False
+
+showMsg :: Command -> Bool
+showMsg (SetCommand _ _ _ _ False) = False
+showMsg QuitCommand = False
+showMsg _ = True
+
+-- | The StatisticsCommand can accept one of three different options.
 data StatisticsOption = StatisticsOptionItems
                       | StatisticsOptionSlabs
                       | StatisticsOptionSizes
