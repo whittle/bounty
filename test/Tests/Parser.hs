a71780333c11f781c47630128ecc16ea0d286976
Jason Whittle <jason.whittle@gmail.com>
2013-07-03 18:40:37 -0400

    Clean up parser tests.

--- test/Tests/Parser.hs
+++ test/Tests/Parser.hs
@@ -17,104 +17,72 @@ import Network.Memcached.Types
 tests = $(testGroupGenerator)
 
 test_set =
-  [ testCase "without noreply" _case_set
-  , testCase "with noreply" _case_set_noreply
+  [ testCase "without noreply"
+    $ "set asdf 1 2 3\r\nqwe\r\n" @<?= Set "asdf" 1 2 True "qwe"
+  , testCase "with noreply"
+    $ "set asdf 1 2 3 noreply\r\nqwe\r\n" @<?= Set "asdf" 1 2 False "qwe"
   ]
-_case_set = assertCommandParse "set asdf 1 2 3\r\nqwe\r\n"
-            $ Set "asdf" 1 2 True "qwe"
-_case_set_noreply = assertCommandParse "set asdf 1 2 3 noreply\r\nqwe\r\n"
-                    $ Set "asdf" 1 2 False "qwe"
 
 test_add =
-  [ testCase "without noreply" _case_add
-  , testCase "with noreply" _case_add_noreply
+  [ testCase "without noreply"
+    $ "add sdfg 4 5 6\r\nzxcvbn\r\n" @<?= Add "sdfg" 4 5 True "zxcvbn"
+  , testCase "with noreply"
+    $ "add sdfg 4 5 6 noreply\r\nzxcvbn\r\n" @<?= Add "sdfg" 4 5 False "zxcvbn"
   ]
-_case_add = assertCommandParse "add sdfg 4 5 6\r\nzxcvbn\r\n"
-            $ Add "sdfg" 4 5 True "zxcvbn"
-_case_add_noreply = assertCommandParse "add sdfg 4 5 6 noreply\r\nzxcvbn\r\n"
-                    $ Add "sdfg" 4 5 False "zxcvbn"
 
 test_replace =
-  [ testCase "without noreply" _case_replace
-  , testCase "with noreply" _case_replace_noreply
+  [ testCase "without noreply"
+    $ "replace dfgh 7 8 9\r\nqwertyuio\r\n" @<?= Replace "dfgh" 7 8 True "qwertyuio"
+  , testCase "with noreply"
+    $ "replace dfgh 7 8 9 noreply\r\nqwertyuio\r\n" @<?= Replace "dfgh" 7 8 False "qwertyuio"
   ]
-_case_replace = assertCommandParse "replace dfgh 7 8 9\r\nqwertyuio\r\n"
-                $ Replace "dfgh" 7 8 True "qwertyuio"
-_case_replace_noreply = assertCommandParse "replace dfgh 7 8 9 noreply\r\nqwertyuio\r\n"
-                        $ Replace "dfgh" 7 8 False "qwertyuio"
 
 test_append =
-  [ testCase "without noreply" _case_append
-  , testCase "with noreply" _case_append_noreply
+  [ testCase "without noreply"
+    $ "append fghj 10 11 12\r\nqwertyuiop[]\r\n" @<?= Append "fghj" 10 11 True "qwertyuiop[]"
+  , testCase "with noreply"
+    $ "append fghj 10 11 12 noreply\r\nqwertyuiop[]\r\n" @<?= Append "fghj" 10 11 False "qwertyuiop[]"
   ]
-_case_append = assertCommandParse "append fghj 10 11 12\r\nqwertyuiop[]\r\n"
-               $ Append "fghj" 10 11 True "qwertyuiop[]"
-_case_append_noreply = assertCommandParse "append fghj 10 11 12 noreply\r\nqwertyuiop[]\r\n"
-                       $ Append "fghj" 10 11 False "qwertyuiop[]"
 
 test_prepend =
-  [ testCase "without noreply" _case_prepend
-  , testCase "with noreply" _case_prepend_noreply
+  [ testCase "without noreply"
+    $ "prepend ghjk 13 14 15\r\nqwertyuizxcvbnm\r\n" @<?= Prepend "ghjk" 13 14 True "qwertyuizxcvbnm"
+  , testCase "with noreply"
+    $ "prepend ghjk 13 14 15 noreply\r\nqwertyuizxcvbnm\r\n" @<?= Prepend "ghjk" 13 14 False "qwertyuizxcvbnm"
   ]
-_case_prepend = assertCommandParse "prepend ghjk 13 14 15\r\nqwertyuizxcvbnm\r\n"
-                $ Prepend "ghjk" 13 14 True "qwertyuizxcvbnm"
-_case_prepend_noreply = assertCommandParse "prepend ghjk 13 14 15 noreply\r\nqwertyuizxcvbnm\r\n"
-                        $ Prepend "ghjk" 13 14 False "qwertyuizxcvbnm"
 
 test_cas =
-  [ testCase "without noreply" _case_cas
-  , testCase "with noreply" _case_cas_noreply
+  [ testCase "without noreply"
+    $ "cas hjkl 16 17 18 19\r\nqwertyuiop[]zxcvbn\r\n" @<?= Cas "hjkl" 16 17 19 True "qwertyuiop[]zxcvbn"
+  , testCase "with noreply"
+    $ "cas hjkl 16 17 18 19 noreply\r\nqwertyuiop[]zxcvbn\r\n" @<?= Cas "hjkl" 16 17 19 False "qwertyuiop[]zxcvbn"
   ]
-_case_cas = assertCommandParse "cas hjkl 16 17 18 19\r\nqwertyuiop[]zxcvbn\r\n"
-            $ Cas "hjkl" 16 17 19 True "qwertyuiop[]zxcvbn"
-_case_cas_noreply = assertCommandParse "cas hjkl 16 17 18 19 noreply\r\nqwertyuiop[]zxcvbn\r\n"
-                    $ Cas "hjkl" 16 17 19 False "qwertyuiop[]zxcvbn"
 
-case_get = assertCommandParse "get rtyu\r\n" $ Get ["rtyu"]
-case_gets = assertCommandParse "gets tyui yuio uiop\r\n"
-            $ Get ["tyui", "yuio", "uiop"]
-case_delete = assertCommandParse "delete jkl;\r\n"
-              $ Delete "jkl;" True
-case_incr = assertCommandParse "incr wert 21\r\n"
-            $ Increment "wert" 21 True
-case_decr = assertCommandParse "decr erty 22 noreply\r\n"
-            $ Decrement "erty" 22 False
-case_touch = assertCommandParse "touch rtyu 23\r\n"
-             $ Touch "rtyu" 23 True
-case_slabs_reassign = assertCommandParse "slabs reassign -1 24\r\n"
-                      $ SlabsReassign (-1) 24
-case_slabs_automove = assertCommandParse "slabs automove 0\r\n"
-                      $ SlabsAutomove 0
+case_get = "get rtyu\r\n" @<?= Get ["rtyu"]
+case_gets = "gets tyui yuio uiop\r\n" @<?= Get ["tyui", "yuio", "uiop"]
+case_delete = "delete jkl;\r\n" @<?= Delete "jkl;" True
+case_incr = "incr wert 21\r\n" @<?= Increment "wert" 21 True
+case_decr = "decr erty 22 noreply\r\n" @<?= Decrement "erty" 22 False
+case_touch = "touch rtyu 23\r\n" @<?= Touch "rtyu" 23 True
+case_slabs_reassign = "slabs reassign -1 24\r\n" @<?= SlabsReassign (-1) 24
+case_slabs_automove = "slabs automove 0\r\n" @<?= SlabsAutomove 0
 
 test_stats =
-  [ testCase "with no argument" _case_stats
-  , testCase "with settings argument" _case_stats_settings
-  , testCase "with items argument" _case_stats_items
-  , testCase "with slabs argument" _case_stats_slabs
-  , testCase "with sizes argument" _case_stats_sizes
+  [ testCase "with no argument" $ "stats\r\n" @<?= Statistics Nothing
+  , testCase "with settings argument" $ "stats settings\r\n" @<?= (Statistics $ Just StatSettings)
+  , testCase "with items argument" $ "stats items\r\n" @<?= (Statistics $ Just StatItems)
+  , testCase "with slabs argument" $ "stats slabs\r\n" @<?= (Statistics $ Just StatSlabs)
+  , testCase "with sizes argument" $ "stats sizes\r\n" @<?= (Statistics $ Just StatSizes)
   ]
-_case_stats = assertCommandParse "stats\r\n"
-              $ Statistics Nothing
-_case_stats_settings = assertCommandParse "stats settings\r\n"
-                       $ Statistics $ Just StatSettings
-_case_stats_items = assertCommandParse "stats items\r\n"
-                    $ Statistics $ Just StatItems
-_case_stats_slabs = assertCommandParse "stats slabs\r\n"
-                    $ Statistics $ Just StatSlabs
-_case_stats_sizes = assertCommandParse "stats sizes\r\n"
-                    $ Statistics $ Just StatSizes
 
 test_flushAll =
-  [ testCase "without an int" _case_flushAll
-  , testCase "with an int" _case_flushAll_int
+  [ testCase "without an int" $ "flush_all\r\n" @<?= FlushAll Nothing True
+  , testCase "with an int" $ "flush_all 23 noreply\r\n" @<?= FlushAll (Just 23) False
   ]
-_case_flushAll = assertCommandParse "flush_all\r\n"
-                 $ FlushAll Nothing True
-_case_flushAll_int = assertCommandParse "flush_all 23 noreply\r\n"
-                     $ FlushAll (Just 23) False
 
 case_version = assertCommandParse "version\r\n" Version
 case_verbosity = assertCommandParse "verbosity 0\r\n" $ Verbosity 0
 case_quit = assertCommandParse "quit\r\n" Quit
 
 assertCommandParse b c = parseOnly command b @?= Right c
+(@<?=) = assertCommandParse
